81d6f9e9acdfe1a7a7f15cb88b14b67e4962c628
Yao Yuan <opal_obsidian@hotmail.com>
2015-10-05 11:11:29 +0100

    refined event query to specify viewports

--- controllers/events.js
+++ controllers/events.js
@@ -2,10 +2,34 @@ var request = require("request");
 
 
 function index(req, res){
+  console.log(typeof req.query.latitude);
+  var latitude = req.query.latitude;
+  var longitude = req.query.longitude;
+  // the viewport amount can be made dynamic, currently the values are for one square mile distance from the centre
+  var NELatitude = Number(latitude) + 0.0005;
+  var NELongitude = Number(longitude) + 0.01;
+  var SWLatitude = Number(latitude) - 0.0005;
+  var SWLongitude = Number(longitude) - 0.01;
+
+  //format current date time into YYYY-MM-DDThh:mm:ss
+  var date = new Date()
+  var currentDateTime = String(date.getUTCFullYear()).concat("-", date.getUTCMonth() + 1, "-", date.getUTCDate(), "T", date.getUTCHours(), ":", date.getUTCMinutes(), ":", date.getUTCSeconds() ) 
+  // date.getUTCFullYear();
+  // date.getUTCMonth() + 1
+  // date.getUTCDate()
+  // date.getUTCHours()
+  // date.getUTCMinutes()
+  // date.getUTCSeconds()
+
+  var url = "https://www.eventbriteapi.com/v3/events/search/?location.latitude=" + latitude +"&location.longitude=" + longitude +"&location.viewport.northeast.latitude=" + NELatitude +"&location.viewport.northeast.longitude=" + NELongitude + "&location.viewport.southwest.latitude="+ SWLatitude + "&location.viewport.southwest.longitude=" + SWLongitude + "&token=" + process.env.EVENTBRITE_PERSONAL_OAUTH_TOKEN
+
+// + "&start_date.range_end=" + currentDateTime 
+// &start_date.range_start=2015-10-03T18:00:00Z&start_date.range_end=2015-10-11T04:00:00Z
+
   console.log(req.query);
-  request("https://www.eventbriteapi.com/v3/events/search/?location.latitude=" + req.query.latitude + "&location.longitude=" + req.query.longitude + "&start_date.range_start=2015-10-03T18:00:00Z&start_date.range_end=2015-10-11T04:00:00Z&token=" + process.env.EVENTBRITE_PERSONAL_OAUTH_TOKEN, function(error, response, body){
+  request(url, function(error, response, body){
     console.log("these are the response: " + response);
-    console.log("the body is: " + typeof response.body);
+    console.log("Number of events is: " + response.body);
     res.json(JSON.parse(response.body));
   })
 
--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ var mongoose = require("mongoose");
 var userSchema = new mongoose.Schema({
   name: String,
   email: String,
-  password: String
+  password: String,
+  events: [{type: mongoose.Schema.ObjectId, ref: "Event"}]
 });
 
 userSchema.methods.getLocation = function(){
