af0944ff6238c66d410fdd1bcbacf1b7320911a4
Yao Yuan <opal_obsidian@hotmail.com>
2015-10-05 14:27:15 +0100

    events are now filtered so that only start dates equals current date will be returned

--- controllers/events.js
+++ controllers/events.js
@@ -5,34 +5,34 @@ function index(req, res){
   console.log(typeof req.query.latitude);
   var latitude = req.query.latitude;
   var longitude = req.query.longitude;
-  // the viewport amount can be made dynamic, currently the values are for one square mile distance from the centre
-  var NELatitude = Number(latitude) + 0.0005;
-  var NELongitude = Number(longitude) + 0.01;
-  var SWLatitude = Number(latitude) - 0.0005;
-  var SWLongitude = Number(longitude) - 0.01;
 
-  //format current date time into YYYY-MM-DDThh:mm:ss
+  //format current date time into YYYY-MM-DD
   var date = new Date()
-  var currentDateTime = String(date.getUTCFullYear()).concat("-", date.getUTCMonth() + 1, "-", date.getUTCDate(), "T", date.getUTCHours(), ":", date.getUTCMinutes(), ":", date.getUTCSeconds() ) 
-  // date.getUTCFullYear();
-  // date.getUTCMonth() + 1
-  // date.getUTCDate()
-  // date.getUTCHours()
-  // date.getUTCMinutes()
-  // date.getUTCSeconds()
+  var currentDate = String(date.getUTCFullYear()).concat("-", date.getUTCMonth() + 1, "-", date.getUTCDate());
 
-  var url = "https://www.eventbriteapi.com/v3/events/search/?location.latitude=" + latitude +"&location.longitude=" + longitude +"&location.viewport.northeast.latitude=" + NELatitude +"&location.viewport.northeast.longitude=" + NELongitude + "&location.viewport.southwest.latitude="+ SWLatitude + "&location.viewport.southwest.longitude=" + SWLongitude + "&token=" + process.env.EVENTBRITE_PERSONAL_OAUTH_TOKEN
-
-// + "&start_date.range_end=" + currentDateTime 
-// &start_date.range_start=2015-10-03T18:00:00Z&start_date.range_end=2015-10-11T04:00:00Z
+  var lineupUrl = "http://planvine.com/api/v1.7/event/?apiKey="+ process.env.LINEUPNOW_API_KEY + "&lat="+ latitude + "&lng=" + longitude + "&radius=1&startDate=" + currentDate + "&order=date";
 
   console.log(req.query);
-  request(url, function(error, response, body){
-    console.log("these are the response: " + response);
-    console.log("Number of events is: " + response.body);
-    res.json(JSON.parse(response.body));
+  request(lineupUrl, function(error, response, body){
+    console.log("the keys of the body: " + Object.keys(JSON.parse(response.body)));
+    console.log(JSON.parse(response.body).data[0].venues);
+    events = JSON.parse(response.body).data.filter(isActive);
+    res.json(events);
   })
+}
 
+// function excludeFutureEvents(events){
+//   events.filter(isActive);
+// }
+
+function isActive(event){
+  console.log("inside isActive function, the type of event is: " + typeof event + "; the keys of event are: " + Object.keys(event));
+  var eventStartDate = new Date(event.venues[0].performances[0].startDate);
+  console.log("this is the start date: " + eventStartDate);
+  var currentDate = new Date();
+  var result = ((eventStartDate.getDate() === currentDate.getDate()) && (eventStartDate.getFullYear() === currentDate.getFullYear()) && (eventStartDate.getMonth() === currentDate.getMonth()));
+  console.log("the boolean result is: " + result);
+  return result;
 }
 
 module.exports = {
