0a77e049ab9dcfead7d92ae7e61a6dfabfd9938f
Yao Yuan <opal_obsidian@hotmail.com>
2015-10-06 23:54:38 +0100

    events can now be pushed into User model

--- controllers/events.js
+++ controllers/events.js
@@ -18,6 +18,12 @@ module.exports = function(io){
         console.log(user);
         if (event) {
           user.events.push(event);
+          user.save(function(err, user){
+            if (err) console.log(err);
+            console.log("event pushed into user");
+          });
+          console.log("event already exists in DB");
+          console.log(user.events);
           event.getChatRoom(io, user, penName);
         } else {
           newEvent = new Event({
@@ -26,8 +32,14 @@ module.exports = function(io){
           })
           newEvent.save(function(err, newEvent){
             if (err) console.log(err);
+            console.log("event being created in DB");
             console.log(user);
             user.events.push(newEvent);
+            user.save(function(err, user){
+              if (err) console.log(err);
+              console.log("event pushed into user");
+            });
+            console.log(user.events);
             newEvent.getChatRoom(io, user, penName);
           })
         }
