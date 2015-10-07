eef6666a3f98aa3852cc626022ced57536b2e35c
Yao Yuan <opal_obsidian@hotmail.com>
2015-10-07 20:13:06 +0100

    tidied up a bit of code in Model

--- models/event.js
+++ models/event.js
@@ -1,55 +1,53 @@
+var mongoose = require("mongoose");
+var Message = require("./message");
 
-  var mongoose = require("mongoose");
-  var Message = require("./message");
+var eventSchema = new mongoose.Schema({
+  title: String,
+  venue: {
+    name: String,
+    Latitude: Number,
+    Longitude: Number
+  },
+  startDate: Date,
+  description: String,
+  lineupId: String
+});
 
-  var eventSchema = new mongoose.Schema({
-    title: String,
-    venue: {
-      name: String,
-      Latitude: Number,
-      Longitude: Number
-    },
-    startDate: Date,
-    description: String,
-    lineupId: String
+eventSchema.methods.getChatRoom = function(io, user, penName){
+  var chatroom = this.id;
+  var eventObject = this;
+  var connected = false;
+  io.on("connection", function (socket) {
+    console.log("this is the socket id: " + socket.id);
+    console.log("is this socket already connected?" + connected);
+    if (!connected) {
+      socket.join(chatroom,function(){
+        connected = true;
+      });
+
+      console.log("join in chatroom: " + chatroom);
+      socket.on('chat message', function (message) {
+        console.log("received message: " + message);
+        var newMessage = new Message({
+          content: message.message,
+          event: eventObject,
+          author: {
+            penName: penName,
+            author_id: user
+          }
+        })
+        newMessage.save(function(err, msg){
+          console.log(msg + "msg has been saved");
+        })
+        io.to(chatroom).emit("chat message", penName + ": " +message.message);
+      });
+    }
   });
+}
 
-  eventSchema.methods.getChatRoom = function(io, user, penName){
-    var chatroom = this.id;
-    var eventObject = this;
-    console.log("the chatroom for the event is: " + chatroom);
-    console.log("type of io inside Event model is: " + io);
-    var connected = false;
-    io.on("connection", function (socket) {
-      // var roster = io.clients(chatroom);
-      // console.log("sockets inside the chatroom: " + roster);
-      console.log("this is the socket id: " + socket.id);
-      console.log("is this socket already connected?" + connected);
-      if (!connected) {
-        socket.join(chatroom,function(){
-          connected = true;
-        });
-
-        console.log("join in chatroom: " + chatroom);
-        socket.on('chat message', function (message) {
-          console.log("received message: " + message);
-          var newMessage = new Message({
-            content: message.message,
-            event: eventObject,
-            author: {
-              penName: penName,
-              author_id: user
-            }
-          })
-          newMessage.save(function(err, msg){
-            console.log(msg + "msg has been saved");
-          })
-          io.to(chatroom).emit("chat message", penName + ": " +message.message);
-        });
-      }
-    });
-  }
+eventSchema.methods.getPastMessages = function (){
+  // Message.find({event: this})
+}
 
 module.exports = mongoose.model("Event", eventSchema);
-  // return mongoose.model("Event", eventSchema);
 
